dacb088894c7820e8f422843fd110654189441f8
2dam <pabsermat@gmail.com>
2024-02-19 11:05:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/composetokens/data/sources/LoginTokens.kt
@@ -0,0 +1,6 @@
+package com.example.composetokens.data.sources
+
+data class LoginTokens(
+    val accesToen: String,
+    val refreshtoken: String
+)
---
+++ app/src/main/java/com/example/composetokens/data/sources/remote/TokenAuthenticator.kt
@@ -0,0 +1,46 @@
+package com.example.composetokens.data.sources.remote
+
+
+import com.example.composetokens.data.TokenManager
+import com.example.composetokens.data.sources.LoginTokens
+import com.example.plantillaexamen.utils.NetworkResult
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import okhttp3.Authenticator
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
+
+class TokenAuthenticator @Inject constructor(private val tokenManager: TokenManager) :
+    Authenticator {
+    override fun authenticate(route: Route?, response: Response): Request {
+        val token = runBlocking { tokenManager.getToken().first()
+        }
+        return runBlocking {
+            val newToken= getNewToken(token)
+            val newAccessToken = newToken.headers()[Constantes.AUTHORIZATION]
+            newAccessToken?.let { tokenManager.saveAccessToken(it) }
+        response.request.newBuilder().header(Constantes.AUTHORIZATION, "$token").build()
+        }
+
+    }
+
+    private suspend fun getNewToken(refreshToken: String?): NetworkResult<LoginTokens> {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        val okHttpClient = OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Constantes.baseurl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+        val service = retrofit.create(AuthService::class.java)
+        return service.refreshToken("$refreshToken")
+    }
+}
